08ad65eb802991b831152a072ff15606ecb1d907
MArKisS6 <playmilokin@gmail.com>
2026-09-07 23:22:56 +0300

    e
    e

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import Category, Location, Post
+# from .models import Category, Location, Post
 
-admin.site.empty_value_display = 'Не задано'
+# admin.site.empty_value_display = 'Не задано'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,26 +1,22 @@
-# from django.shortcuts import render
-# from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 
-# posts_id = {post['id']: post for post in posts}
+def index(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
-# def index(request):
-#    return render(request, 'blog/index.html', {'posts': posts})
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/detail.html', {'post': post})
 
 
-# def post_detail(request, post_id):
-#    post = posts_id.get(post_id)
-#    if post is None:
-#       raise Http404('Запись блога с таким ID не найдена.')
-#    return render(request, 'blog/detail.html', {'post': post})
-
-
-# def category_posts(request, category_slug):
-# return render(
-# request,
-# 'blog/category.html',
-# {
-#     'category_slug': category_slug,
-# }
-#    )
+def category_posts(request, category_slug):
+    return render(
+        request,
+        'blog/category.html',
+        {
+            'category_slug': category_slug
+        }
+    )
