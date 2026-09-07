3ee63c17158083473b6207caf6d24c00e0325cb5
MArKisS6 <playmilokin@gmail.com>
2026-09-07 17:31:14 +0300

    s
    c

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Location(models.Model):
+    pass
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Заголовок'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+
+    )
 
 
 class Post(models.Model):
@@ -17,6 +35,15 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         verbose_name='Автор публикации'
     )
-    
+    location = models.ForeignKey(
+        Location,
+        verbose_name='',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
     def __str__(self):
         return self.title
