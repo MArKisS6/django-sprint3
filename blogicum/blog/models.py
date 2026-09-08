22a82015ce18dc9bed8e2bd376ffac2ce6165841
MArKisS6 <playmilokin@gmail.com>
2026-09-08 20:13:10 +0300

    5
    5

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -29,8 +29,11 @@ class Category(PublishedModel):
     )
     slug = models.SlugField(
         unique=True,
-        verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        verbose_name='Слаг',
+        help_text=(
+            'Идентификатор страницы для URL; разрешены символы латиницы, '
+            'цифры, дефис и подчёркивание.'
+        )
     )
 
     class Meta:
@@ -51,7 +54,10 @@ class Post(PublishedModel):
     )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
+        help_text=(
+            'Если установить дату и время в будущем — можно делать '
+            'отложенные публикации.'
+        )
     )
     author = models.ForeignKey(
         User,
