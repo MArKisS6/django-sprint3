805a842b9d32ea5dac5d8446f6377d3aee474e0b
MArKisS6 <playmilokin@gmail.com>
2026-09-08 09:47:59 +0300

    Update admin.py

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,5 +1,29 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import Category, Location, Post
+from .models import Category, Location, Post
 
-# admin.site.empty_value_display = 'Не задано'
+admin.site.empty_value_display = 'Не задано'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'pub_date',
+        'is_published',
+        'category',
+        'author',
+        'location',
+        'created_at'
+    )
+    list_editable = (
+        'is_published',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('is_published',)
+    list_display_links = ('title',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Location)
