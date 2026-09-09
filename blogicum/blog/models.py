4ec30ab87fd3ec430b2e3c2b790efe1a64f67916
MArKisS6 <playmilokin@gmail.com>
2026-09-09 10:09:57 +0300

    Update models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -2,12 +2,27 @@ import textwrap
 from django.db import models
 from django.contrib.auth import get_user_model
 
-from core.models import PublishedModel
-
 User = get_user_model()
 
 MAX_LEN = 256
 MAX_STR = 50
+
+class PublishedModel(models.Model):
+    """Абстрактная модель. Добавляет флаг is_published и created_at."""
+
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        abstract = True
+
 
 class Location(PublishedModel):
     name = models.CharField(
@@ -25,8 +40,6 @@ class Location(PublishedModel):
             width=MAX_STR, 
             placeholder='...'
         )
-
-
 
 
 class Category(PublishedModel):
